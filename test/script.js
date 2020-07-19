5ce07e0bfd4f41eca83d6a6a023d6b522b260544
ozzychel <andrey.petunin86@gmail.com>
2020-07-19 16:55:11 -0700

    add new relic, perform tests

--- test/script.js
+++ test/script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 4000,
+  vus: 2000,
   duration: '60s'
 }
 
@@ -10,7 +10,7 @@ export default function() {
   let random = Math.floor(Math.random() * (10000000)) + 1;
   let res = http.get(`http://localhost:3003/api/users/${random}`);
   check(res, { 'status was 200': r => r.status == 200 });
-  sleep(1)
+  sleep(1);
 }
 
 
